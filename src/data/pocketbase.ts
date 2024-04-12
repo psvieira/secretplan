ea313723496bdf93d5b6864363e1e2ca9514d527
psvieira <pedro.vieira@mac.com>
2024-04-12 12:26:19 +0100

    Create teams

--- src/data/pocketbase.ts
+++ src/data/pocketbase.ts
@@ -163,3 +163,28 @@ export function processImages(task: TasksResponse) {
 
   return images
 }
+
+export async function addTeam(name: string) {
+  let team = await pb.collection('teams').create({
+    name,
+    created_by: pb.authStore.model?.id,
+    status: 'inactive',
+  })
+
+  return team
+}
+
+export async function getTeam(id: string) {
+  const team = await pb.collection('teams').getOne(id)
+
+  return team
+}
+
+export async function userIsTeamOwner(team_id: string) {
+  const team = await getTeam(team_id)
+  if (team.created_by === pb.authStore.model?.id) {
+    return true
+  }
+  return false
+}
+
